95f627585b7c55c3e53caa9d3fdb7c9e2550ae54
mattmc3 <mattmc3@gmail.com>
2021-11-08 16:29:22 -0500

    Compare with xontrib-cookiecutter

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
     description="Go up any number of directories.",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    python_requires='>=3.7',
+    python_requires='>=3.6',
+    install_requires=['xonsh'],
     packages=['xontrib'],
     package_dir={'xontrib': 'xontrib'},
     package_data={'xontrib': ['*.xsh']},
@@ -27,16 +28,15 @@ setup(
         "Code": "https://github.com/oh-my-xonsh/xontrib-up",
         "Issue tracker": "https://github.com/oh-my-xonsh/xontrib-up/issues",
     },
-    zip_safe=False,
     classifiers=[
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Unix Shell",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         "Topic :: System :: Shells",
         "Topic :: System :: System Shells",
         "Topic :: Terminals",
-        "License :: OSI Approved :: MIT License"
     ]
 )
